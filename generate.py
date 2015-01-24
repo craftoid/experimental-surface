ffccc702eae40fbc4979852d58b9bd9630a812c0
Anders Hoff <inconvergent@gmail.com>
2015-01-24 14:40:32 +0100

    removed bad base meshes.

--- generate.py
+++ generate.py
@@ -243,7 +243,7 @@ def main():
 
   from time import time
 
-  steps = 200
+  steps = 50
 
   noise = 0.0008
   stp_attract = 0.02
@@ -267,7 +267,9 @@ def main():
       S.step()
       itt = S.itt
       if not itt%10:
-        print(itt)
+        fnitt = './res/{:s}_{:05d}.blend'.format(out_fn,itt)
+        S.save(fnitt)
+        print(fnitt)
 
     except KeyboardInterrupt:
       print('KeyboardInterrupt')
